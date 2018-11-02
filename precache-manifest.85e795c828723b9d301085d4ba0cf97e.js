a2f93c006bfafb58cff44e94b6cf7c1ce823ac2f
Pietro Passarelli - News Labs <pietro.passarelli@bbc.co.uk>
2018-11-02 15:32:27 +0000

    Updates

---
+++ precache-manifest.85e795c828723b9d301085d4ba0cf97e.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = [
+  {
+    "revision": "4a686d48d5a089750c49",
+    "url": "./static/js/runtime~main.4a686d48.js"
+  },
+  {
+    "revision": "b84aaa3f21781d56994f",
+    "url": "./static/js/main.b84aaa3f.chunk.js"
+  },
+  {
+    "revision": "e15ac1d868171adbd8c3",
+    "url": "./static/js/1.e15ac1d8.chunk.js"
+  },
+  {
+    "revision": "b84aaa3f21781d56994f",
+    "url": "./static/css/main.3ee238d7.chunk.css"
+  },
+  {
+    "revision": "5ff1749340a1e90e2ea5ef0eaadb9130",
+    "url": "./index.html"
+  }
+];
--- service-worker.js
+++ service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/3.6.3/workbox-sw.js");
 
 importScripts(
-  "/react-transcript-editor/precache-manifest.ccc2e1d484b464c79c0d1db8f36a9498.js"
+  "./precache-manifest.85e795c828723b9d301085d4ba0cf97e.js"
 );
 
 workbox.clientsClaim();
@@ -28,7 +28,7 @@ self.__precacheManifest = [].concat(self.__precacheManifest || []);
 workbox.precaching.suppressWarnings();
 workbox.precaching.precacheAndRoute(self.__precacheManifest, {});
 
-workbox.routing.registerNavigationRoute("/react-transcript-editor/index.html", {
+workbox.routing.registerNavigationRoute("./index.html", {
   
   blacklist: [/^\/_/,/\/[^\/]+\.[^\/]+$/],
 });
