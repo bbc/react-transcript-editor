60bea99767ed8315d7f3a262af21fe81961ede76
Pietro Passarelli - News Labs <pietro.passarelli@bbc.co.uk>
2018-11-06 18:28:22 +0000

    enable two tests for media player

--- src/lib/TranscriptEditor/MediaPlayer/index.test.js
+++ src/lib/TranscriptEditor/MediaPlayer/index.test.js
@@ -18,9 +18,7 @@ test('GIVEN a chapter title I expect that WHEN the Video component is rendered T
 test("GIVEN a video as a chapter with src video url THEN the video is rendered with it's source url", () => {
   const { getByTestId } = render(<MediaPlayer mediaUrl={ fakeVideoUrl } />);
 
-  expect(getByTestId('media-player-id').attributes.src.value).toBe(
-    fakeVideoUrl
-  );
+  expect(getByTestId('media-player-id').attributes.src.value).toBe(fakeVideoUrl);
 });
 xtest('WHEN the Video component is rendered THEN a video element is displayed', () => {
   const wrapper = shallow(<MediaPlayer mediaUrl={ fakeVideoUrl } />);
