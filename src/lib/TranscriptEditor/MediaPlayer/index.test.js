a1f3ac80063b6e2b63eaa7f1f21bf817faa2da30
Pietro Passarelli - News Labs <pietro.passarelli@bbc.co.uk>
2018-11-06 18:28:18 +0000

    enable two tests for media player

--- src/lib/TranscriptEditor/MediaPlayer/index.test.js
+++ src/lib/TranscriptEditor/MediaPlayer/index.test.js
@@ -9,20 +9,19 @@ afterEach(cleanup);
 const fakeVideoUrl =
   'https://storage.googleapis.com/coverr-main/mp4/Pigeon-Impossible.mp4';
 
-xtest('GIVEN a chapter title I expect that WHEN the Video component is rendered THEN the correct title is displayed', () => {
+test('GIVEN a chapter title I expect that WHEN the Video component is rendered THEN the correct title is displayed', () => {
   const { container } = render(<MediaPlayer mediaUrl={ fakeVideoUrl } />);
 
   expect(container.innerHTML).toContain('videoSection');
 });
 
-xtest("GIVEN a video as a chapter with src video url THEN the video is rendered with it's source url", () => {
+test("GIVEN a video as a chapter with src video url THEN the video is rendered with it's source url", () => {
   const { getByTestId } = render(<MediaPlayer mediaUrl={ fakeVideoUrl } />);
 
   expect(getByTestId('media-player-id').attributes.src.value).toBe(
     fakeVideoUrl
   );
 });
-
 xtest('WHEN the Video component is rendered THEN a video element is displayed', () => {
   const wrapper = shallow(<MediaPlayer mediaUrl={ fakeVideoUrl } />);
   expect(wrapper.find('video').type()).toBe('video');
