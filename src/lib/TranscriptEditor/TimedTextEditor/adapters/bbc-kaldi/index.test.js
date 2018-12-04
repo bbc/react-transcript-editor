13f9433d579b6c7fe301e1bfd6d78c2e56d697b0
Pietro Passarelli - News Labs <pietro.passarelli@bbc.co.uk>
2018-12-04 18:17:03 +0000

    change

--- src/lib/TranscriptEditor/TimedTextEditor/adapters/bbc-kaldi/index.test.js
+++ src/lib/TranscriptEditor/TimedTextEditor/adapters/bbc-kaldi/index.test.js
@@ -3,6 +3,8 @@ import bbcKaldiToDraft from './index';
 import draftTranscriptExample from './sample/bbcKaldiToDraft-sample';
 import kaldiTedTalkTranscript from './sample/kaldiTedTalkTranscript-sample.json';
 
+// TODO: figure out why the second of these two tests hang 
+// might need to review the draftJS data structure output
 describe('bbcKaldiToDraft', () => {
   const result = bbcKaldiToDraft(kaldiTedTalkTranscript);
   it.skip('Should be defined', ( ) => {
