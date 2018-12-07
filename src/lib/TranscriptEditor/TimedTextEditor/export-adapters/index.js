963d9bbeb90afdbbe70d9806f5a010f0cedb67c2
Pietro <pietro.passarelli@gmail.com>
2018-12-07 10:23:25 +0000

    Pretty print for DraftJS export
    Co-Authored-By: chrisbaume <chrisbaume@gmail.com>

--- src/lib/TranscriptEditor/TimedTextEditor/export-adapters/index.js
+++ src/lib/TranscriptEditor/TimedTextEditor/export-adapters/index.js
@@ -8,7 +8,7 @@ import draftToTxt from './txt/index';
 const exportAdapter = (blockData, exportFormat) => {
   switch (exportFormat) {
     case 'draftjs':
-      return { data: JSON.stringify(blockData), ext: 'json' };
+      return { data: JSON.stringify(blockData, null,2), ext: 'json' };
     case 'txt':
       return { data: draftToTxt(blockData), ext: 'txt' };
     default:
