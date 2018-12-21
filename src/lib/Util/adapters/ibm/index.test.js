7f6b4584845234377da8d9e4e17918204b5aa745
Andrew D. Anderson <AndrewDAnderson@gmail.com>
2018-12-21 03:37:00 -0600

    Fix IBM TTS index.test.js

--- src/lib/Util/adapters/ibm/index.test.js
+++ src/lib/Util/adapters/ibm/index.test.js
@@ -1,12 +1,12 @@
-import bbcKaldiToDraft from './index';
+import ibmToDraft from './index';
 
-import draftTranscriptExample from './sample/bbcKaldiToDraft.sample.js';
-import kaldiTedTalkTranscript from './sample/kaldiTedTalkTranscript.sample.json';
+import draftTranscriptExample from './sample/ibmToDraft.sample.js';
+import ibmTedTalkTranscript from './sample/ibmTedTalkTranscript.sample.json';
 
 // TODO: figure out why the second of these two tests hang 
 // might need to review the draftJS data structure output
 describe('bbcKaldiToDraft', () => {
-  const result = bbcKaldiToDraft(kaldiTedTalkTranscript);
+  const result = ibmToDraft(ibmTedTalkTranscript);
   it('Should be defined', ( ) => {
     expect(result).toBeDefined();
   });
