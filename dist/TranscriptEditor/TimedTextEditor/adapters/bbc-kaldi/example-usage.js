cd3085033dc34207b0a3d28b143eddd6c07a5944
Pietro Passarelli - News Labs <pietro.passarelli@bbc.co.uk>
2019-01-03 14:56:46 +0100

    increased version
    using semantic version to make new release and added ignore using babel 7 syntax

--- dist/TranscriptEditor/TimedTextEditor/adapters/bbc-kaldi/example-usage.js
+++
@@ -1,7 +0,0 @@
-const bbcKaldiToDraft = require('./index'); // using require, because of testing outside of React app
-
-
-const kaldiTedTalkTranscript = require('../../../../../sample-data/KateDarling_2018S-bbc-kaldi.json');
-
-const result = bbcKaldiToDraft(kaldiTedTalkTranscript);
-console.log(result);
