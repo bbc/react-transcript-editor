6ea7d9531e8617a3857b0e2aa77692d5769aec74
Joel Gribble <gjoel@amazon.com>
2019-03-18 11:04:32 +1100

    cleaned up spacing in test.js

--- src/lib/Util/adapters/amazon-transcribe/index.test.js
+++ src/lib/Util/adapters/amazon-transcribe/index.test.js
@@ -6,6 +6,7 @@ import amazonTranscribeToDraft, {
 } from './index';
 import amazonTranscribeTedTalkTranscript from './sample/amazonTranscribe.sample.json';
 import draftTranscriptSample from './sample/amazonTranscribe.sample.js';
+
 describe.skip('amazonTranscribeToDraft', () => {
   const result = amazonTranscribeToDraft(amazonTranscribeTedTalkTranscript);
   it('Should be defined', () => {
