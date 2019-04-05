eb40b0da10dc60f0c1673537fc4e7bebb420bdc6
Pietro Passarelli - News Labs <pietro.passarelli@bbc.co.uk>
2019-04-05 15:58:15 +0100

    changed folder structure

--- .storybook/config.js
+++ .storybook/config.js
@@ -1,10 +1,11 @@
 import { configure } from "@storybook/react";
 
 // automatically import all files ending in *.stories.js
-const packages = require.context("../packages/", true, /.stories.js$/);
+// https://webpack.js.org/guides/dependency-management/
+const components = require.context("../packages/components/", true, /.stories.js$/);
 // const demo = require.context("../demo/", true, /.stories.js$/);
 
 configure(() => {
-  packages.keys().forEach(filename => packages(filename));
+  components.keys().forEach(filename => components(filename));
   // demo.keys().forEach(filename => demo(filename));
 }, module);
