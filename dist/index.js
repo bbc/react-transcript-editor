8062731d94fe037f2640a5a3c233bd0f7e035379
Pietro Passarelli - News Labs <pietro.passarelli@bbc.co.uk>
2019-04-05 19:32:37 +0100

    working

--- dist/index.js
+++
@@ -1,1 +0,0 @@
-"use strict";var _index=_interopRequireDefault(require("./TranscriptEditor/index.js"));Object.defineProperty(exports,"__esModule",{value:!0}),Object.defineProperty(exports,"TranscriptEditor",{enumerable:!0,get:function get(){return _index.default}});function _interopRequireDefault(obj){return obj&&obj.__esModule?obj:{default:obj}}
