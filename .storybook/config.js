fcec20f9c4d7b141e86eb237121c4e051d217af3
Pietro Passarelli - News Labs <pietro.passarelli@bbc.co.uk>
2019-04-05 23:01:48 +0100

    Added support for demo app in storybook

--- .storybook/config.js
+++ .storybook/config.js
@@ -3,9 +3,9 @@ import { configure } from "@storybook/react";
 // automatically import all files ending in *.stories.js
 // https://webpack.js.org/guides/dependency-management/
 const components = require.context("../packages/components/", true, /.stories.js$/);
-// const demo = require.context("../demo/", true, /.stories.js$/);
+const demo = require.context("../demo/", true, /.stories.js$/);
 
 configure(() => {
   components.keys().forEach(filename => components(filename));
-  // demo.keys().forEach(filename => demo(filename));
+  demo.keys().forEach(filename => demo(filename));
 }, module);
