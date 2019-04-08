d875026b8c1c0791a155b5614d1af71646811442
Pietro Passarelli - News Labs <pietro.passarelli@bbc.co.uk>
2019-04-08 12:27:47 +0100

    bringing back style lint, and fixing lint in storybook config

--- .storybook/config.js
+++ .storybook/config.js
@@ -2,8 +2,8 @@ import { configure } from "@storybook/react";
 
 // automatically import all files ending in *.stories.js
 // https://webpack.js.org/guides/dependency-management/
-const components = require.context("../packages/components/", true, /.stories.js$/);
-const demo = require.context("../demo/", true, /.stories.js$/);
+const components = require.context('../packages/components/', true, /.stories.js$/);
+const demo = require.context('../demo/', true, /.stories.js$/);
 
 configure(() => {
   components.keys().forEach(filename => components(filename));
