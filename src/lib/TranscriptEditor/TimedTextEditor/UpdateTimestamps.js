2d1619baee15544fb6d138beaf4c58b26183ad03
murezzda <47388020+murezzda@users.noreply.github.com>
2019-04-08 12:26:45 +0200

    Rewrote timestamp alignment and differ to be integrated in each other instead of doing a 2-step process.

--- src/lib/TranscriptEditor/TimedTextEditor/UpdateTimestamps.js
+++ src/lib/TranscriptEditor/TimedTextEditor/UpdateTimestamps.js
@@ -25,77 +25,6 @@ const createEntity = (start, end, confidence, word, wordIdx) => {
   });
 };
 
-const realignTimestamps = (differences, currentContent, referenceContent) => {
-
-  var diffIdx = 0;
-  var entityIdx = 0;
-
-  const entities = referenceContent.entityMap;
-  const results = [];
-
-  for (var blockIdx in currentContent.blocks) {
-    const block = currentContent.blocks[blockIdx];
-    const words = block.text.match(/\S+/g) || [];
-
-    var substitutionStart = null;
-    var wordMeta = null;
-    var entity = null;
-
-    if (words.length > 0) {
-      var wordMetaArray = [];
-      var wordIdx = 0;
-
-      while (wordIdx < words.length) {
-        const word = words[wordIdx];
-        const diffType = differences[diffIdx];
-        diffIdx++;
-        if (diffType === 'm' || diffType === 's') {
-          entity = entities[entityIdx].data;
-          wordMeta = createEntity(entity.start, entity.end, entity.confidence, word, wordIdx);
-          wordIdx++;
-          entityIdx++;
-          wordMetaArray.push(wordMeta);
-        } else if (diffType === 'ss') {
-          substitutionStart = entities[entityIdx].data.start;
-          entityIdx++;
-        } else if (diffType === 'se') {
-          wordMeta = createEntity(substitutionStart, entities[entityIdx].data.end, 0.0, word, wordIdx);
-          wordIdx++;
-          entityIdx++;
-          wordMetaArray.push(wordMeta);
-        } else if (diffType === 'd' || diffType === 'si') {
-          entityIdx++;
-        } else if (diffType === 'i') {
-          entity = entities[entityIdx].data;
-          wordMeta = createEntity(entity.start, entity.end, entity.confidence, word, wordIdx);
-          wordIdx++;
-          wordMetaArray.push(wordMeta);
-        } else {
-          console.log('Found illegal symbol ' + diffType);
-          wordIdx++;
-        }
-      }
-
-      const updatedBlock = {
-        text: wordMetaArray.map((entry) => entry.punct).join(' '),
-        type: 'paragraph',
-        data: {
-          speaker: block.data.speaker,
-          words: wordMetaArray,
-          start: wordMetaArray[0].start
-        },
-        entityRanges: generateEntitiesRanges(wordMetaArray, 'punct'),
-      };
-
-      results.push(updatedBlock);
-    }
-  }
-
-  const updatedContent = { blocks: results, entityMap: createEntityMap(results) };
-
-  return updatedContent;
-};
-
 // https://github.com/google/diff-match-patch/wiki/Line-or-Word-Diffs
 const diffLineMode = (text1, text2) => {
   var dmp = new DiffMatchPatch();
@@ -109,70 +38,89 @@ const diffLineMode = (text1, text2) => {
   return diffs;
 };
 
-const diff = (text1, text2) => {
-
-  var diffArray = [];
-  var arrayIdx = 0;
-
-  const lineModeDiff = diffLineMode(text2.join('\n') + '\n', text1.join('\n') + '\n');
-  while (arrayIdx < lineModeDiff.length) {
-    const diffEntry = lineModeDiff[arrayIdx];
-    const numberOfWords = (diffEntry[1].match(/\n/g) || []).length;
-    if (diffEntry[0] === 0) {
-      for (var i = 0; i < numberOfWords; i++) { diffArray.push('m'); }
-    } else if (diffEntry[0] === 1) {
-      for (var i = 0; i < numberOfWords; i++) { diffArray.push('i'); }
-    } else if (diffEntry[0] === -1) {
-      if (arrayIdx < lineModeDiff.length - 1 && lineModeDiff[arrayIdx + 1][0] === 1) {
-        // The matching number of words is substituted
-        const numberOfWordsSub = (lineModeDiff[arrayIdx + 1][1].match(/\n/g) || []).length;
-
-        for (var subItr = 0; subItr < Math.min(numberOfWords, numberOfWordsSub); subItr++) { diffArray.push('s'); }
-        for (var delItr = 0; delItr < numberOfWords - numberOfWordsSub; delItr++) { diffArray.push('d'); }
-        for (var insItr = 0; insItr < numberOfWordsSub - numberOfWords; insItr++) { diffArray.push('i'); }
-
-        // if (numberOfWordsSub < numberOfWords) {
-        //   diffArray.push('ss');
-        //   for (var i = 1; i < (numberOfWords - 1); i++) { diffArray.push('si'); }
-        //   diffArray.push('se');
-        //   for (var i = 0; i < (numberOfWords - numberOfWordsSub); i++) { diffArray.push('i'); }
-        // } else {
-        // }
-
-        // diffArray.push('ss');
-        // for (var i = 1; i < (numberOfWords - 1); i++) { diffArray.push('si'); }
-        // diffArray.push('se');
-        // if (numberOfWords > numberOfWordsSub) {
-        //   // If there are more words in the original substring, delete the rest.
-        //   for (var i = 0; i < (numberOfWords - numberOfWordsSub); i++) { diffArray.push('i'); }
-        // }
-        arrayIdx++; // Increase an additional time to skip insert/delete syntax for substitution
-      } else {
-        for (var i = 0; i < numberOfWords; i++) { diffArray.push('diff'); }
-      }
-    }
-    arrayIdx++;
-  }
-
-  return diffArray;
-};
-
-const diffAndRealign = (currentText, originalText) => {
-  const lineModeDiff = diffLineMode(originalText.join('\n') + '\n', currentText.join('\n') + '\n');
-
-  return lineModeDiff;
-};
-
 const updateTimestamps = (currentContent, originalContent) => {
   const currentText = convertContentToText(currentContent);
   const originalText = convertContentToText(originalContent);
 
-  // const updatedContent = diffAndRealign(currentText, originalText)
+  const lineModeDiff = diffLineMode(originalText.join('\n') + '\n', currentText.join('\n') + '\n');
+  const entities = originalContent.entityMap;
 
-  const diffArray = diff(currentText, originalText);
-  const updatedContent = realignTimestamps(diffArray, currentContent, originalContent);
+  var currentTextIdx = 0;
+  var entityIdx = 0;
+  var diffIdx = 0;
 
-  return (updatedContent);
+  var newEntities = [];
+
+  while (diffIdx < lineModeDiff.length) {
+    const diffEntry = lineModeDiff[diffIdx];
+    const nextDiffEntry = lineModeDiff[diffIdx + 1] || -1;
+    const diffType = diffEntry[0];
+    const numberOfWords = (diffEntry[1].match(/\n/g) || []).length;
+
+    if (diffType === 0) {
+      for (var wordItr = 0; wordItr < numberOfWords; wordItr++) {
+        const word = currentText[currentTextIdx++];
+        const entity = entities[entityIdx++].data;
+
+        const newEntity = createEntity(entity.start, entity.end, 0.0, word, -1);
+        newEntities.push(newEntity);
+      }
+    } else if (diffType === -1) {
+      if (nextDiffEntry !== -1 && nextDiffEntry[0] === 1) {
+        const entityStart = entities[entityIdx].data.start;
+        const entityEnd = entities[entityIdx + numberOfWords - 1].data.end;
+
+        const numberOfReplacements = (nextDiffEntry[1].match(/\n/g) || []).length;
+
+        for (var wordItr = 0; wordItr < numberOfReplacements; wordItr++) {
+          const word = currentText[currentTextIdx++];
+
+          const newEntity = createEntity(entityStart, entityEnd, 0.0, word, -1);
+          newEntities.push(newEntity);
+        }
+        entityIdx += numberOfWords;
+        diffIdx++;
+      } else {
+        entityIdx += numberOfWords;
+      }
+    } else if (diffType === 1) {
+      for (var wordItr = 0; wordItr < numberOfWords; wordItr++) {
+        const word = currentText[currentTextIdx++];
+        const entity = entities[entityIdx].data;
+
+        const newEntity = createEntity(entity.start, entity.end, 0.0, word, -1);
+        newEntities.push(newEntity);
+      }
+    }
+    diffIdx ++;
+  }
+
+  var updatedBlockArray = [];
+  var totalWords = 0;
+
+  for (var blockIdx in currentContent.blocks) {
+    const block = currentContent.blocks[blockIdx];
+    const wordsInBlock = (block.text.match(/\S+/g) || []).length;
+    const blockEntites = newEntities.slice(totalWords, totalWords + wordsInBlock);
+
+    const updatedBlock = {
+      text: blockEntites.map((entry) => entry.punct).join(' '),
+      type: 'paragraph',
+      data: {
+        speaker: block.data.speaker,
+        words: blockEntites,
+        start: blockEntites[0].start
+      },
+      entityRanges: generateEntitiesRanges(blockEntites, 'punct'),
+    };
+
+    updatedBlockArray.push(updatedBlock);
+    totalWords += wordsInBlock;
+  }
+
+  const updatedContent = { blocks: updatedBlockArray, entityMap: createEntityMap(updatedBlockArray) };
+
+  return updatedContent;
 };
 
 export default updateTimestamps;
