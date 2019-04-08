9890718889d9eaa3e0babdc636f112dd9b3075ee
James Dooley <james.dooley@bbc.co.uk>
2019-04-08 17:38:40 +0100

    Feature: Added custom css loading to storybook

--- .storybook/config.js
+++ .storybook/config.js
@@ -1,11 +1,22 @@
-import { configure } from "@storybook/react";
+import { configure } from '@storybook/react';
+import { setOptions } from '@storybook/addon-options';
 
 // automatically import all files ending in *.stories.js
 // https://webpack.js.org/guides/dependency-management/
 const components = require.context('../packages/components/', true, /.stories.js$/);
 const demo = require.context('../demo/', true, /.stories.js$/);
+const styles = require.context('./styles', true, /\.scss$/);
 
-configure(() => {
-  components.keys().forEach(filename => components(filename));
-  demo.keys().forEach(filename => demo(filename));
-}, module);
+function loadStories() {
+  demo.keys().forEach((filename) => demo(filename));
+  components.keys().forEach((filename) => components(filename));
+  styles.keys().forEach((filename) => styles(filename));
+}
+
+setOptions({
+  name: 'react-transcript-editor',
+  url: 'https://github.com/bbc/react-transcript-editor',
+  addonPanelInRight: true
+});
+
+configure(loadStories, module);
