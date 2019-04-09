3589d4090e95d769645dd0f120b606ba5d0f2d6f
Pietro Passarelli - News Labs <pietro.passarelli@bbc.co.uk>
2019-04-09 15:08:50 +0100

    added mediaUrl knob to timedTextEditor

--- packages/components/timed-text-editor/stories/index.stories.js
+++ packages/components/timed-text-editor/stories/index.stories.js
@@ -2,29 +2,35 @@ import React from 'react';
 
 import { storiesOf } from '@storybook/react';
 import { action } from '@storybook/addon-actions';
+import { withKnobs, text, number } from '@storybook/addon-knobs';
 
 import bbcKaldiTranscript from './fixtures/bbc-kaldi.json';
 
 import TimedTextEditor from '../index.js';
 
-const fixtureProps = {
-  transcriptData: bbcKaldiTranscript,
-  mediaUrl: 'https://download.ted.com/talks/KateDarling_2018S-950k.mp4',
-  isEditable: true,
-  onWordClick: action('onWordClick'),
-  sttJsonType: 'bbckaldi',
-  isPlaying: action('isPlaying'),
-  playMedia: action('playMedia'),
-  currentTime: 0,
-  isScrollIntoViewOn: true,
-  isPauseWhileTypingOn: true,
-  timecodeOffset: 0,
-  handleAnalyticsEvents: action('handleAnalyticsEvents'),
-  showSpeakers: true,
-  showTimecodes: true,
-  fileName: 'KateDarling_2018S-950k.mp4'
-};
+storiesOf('TimedTextEditor', module)
+  .addDecorator(withKnobs)
+  .add('default', () => {
+    const mediaUrl = 'https://download.ted.com/talks/KateDarling_2018S-950k.mp4';
 
-storiesOf('TimedTextEditor', module).add('default', () => (
-  <TimedTextEditor { ...fixtureProps } />
-));
+    const fixtureProps = {
+      transcriptData: bbcKaldiTranscript,
+      mediaUrl: text('mediaUrl', mediaUrl),
+      isEditable: true,
+      onWordClick: action('onWordClick'),
+      sttJsonType: 'bbckaldi',
+      isPlaying: action('isPlaying'),
+      playMedia: action('playMedia'),
+      currentTime: 0,
+      isScrollIntoViewOn: true,
+      isPauseWhileTypingOn: true,
+      timecodeOffset: 0,
+      handleAnalyticsEvents: action('handleAnalyticsEvents'),
+      showSpeakers: true,
+      showTimecodes: true,
+      fileName: 'KateDarling_2018S-950k.mp4'
+    };
+
+    return (
+      <TimedTextEditor { ...fixtureProps } />
+    );});
