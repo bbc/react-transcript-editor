9008d1ba8c7625e0dd0d06171d7c8146023ef0e6
Pietro Passarelli - News Labs <pietro.passarelli@bbc.co.uk>
2019-04-09 13:27:39 +0100

    added viewport addon

--- .storybook/addons.js
+++ .storybook/addons.js
@@ -1,4 +1,5 @@
 import '@storybook/addon-knobs/register';
+import '@storybook/addon-viewport/register';
 import '@storybook/addon-options/register';
 import '@storybook/addon-actions/register';
 import '@storybook/addon-links/register';
