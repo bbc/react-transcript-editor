cab1176aa73a7e78c7986dd7ffa0a0553cb762cf
Pietro Passarelli - News Labs <pietro.passarelli@bbc.co.uk>
2019-04-09 14:57:10 +0100

    added settings knobs

--- packages/components/media-player/stories/index.stories.js
+++ packages/components/media-player/stories/index.stories.js
@@ -2,7 +2,7 @@ import React from 'react';
 
 import { storiesOf } from '@storybook/react';
 import { action } from '@storybook/addon-actions';
-import { withKnobs, text, boolean, number } from '@storybook/addon-knobs';
+import { withKnobs, text, number } from '@storybook/addon-knobs';
 
 import MediaPlayer from '../index.js';
 
