8babce7573ba0abebe22101267a36e1e8ecc332a
murezzda <47388020+murezzda@users.noreply.github.com>
2019-04-15 14:29:33 +0200

    added updateTimestampsSSTAlign which updates the timestamps with the sst-align code

--- packages/components/timed-text-editor/UpdateTimestamps/index.js
+++ packages/components/timed-text-editor/UpdateTimestamps/index.js
@@ -1,6 +1,7 @@
-import generateEntitiesRanges from '../../stt-adapters/generate-entities-ranges/index.js';
-import { createEntityMap } from '../../stt-adapters/index.js';
+import generateEntitiesRanges from '../../../stt-adapters/generate-entities-ranges/index.js';
+import { createEntityMap } from '../../../stt-adapters/index.js';
 import DiffMatchPatch from 'diff-match-patch';
+import alignJSONText from './stt-align-node.js';
 
 const convertContentToText = (content) => {
   var text = [];
@@ -25,6 +26,34 @@ const createEntity = (start, end, confidence, word, wordIdx) => {
   });
 };
 
+const createContentFromEntityList = (currentContent, newEntities) => {
+  // Update entites to block structure.
+  var updatedBlockArray = [];
+  var totalWords = 0;
+
+  for (var blockIdx in currentContent.blocks) {
+    const block = currentContent.blocks[blockIdx];
+    const wordsInBlock = (block.text.match(/\S+/g) || []).length;
+    const blockEntites = newEntities.slice(totalWords, totalWords + wordsInBlock);
+
+    const updatedBlock = {
+      text: blockEntites.map((entry) => entry.punct).join(' '),
+      type: 'paragraph',
+      data: {
+        speaker: block.data.speaker,
+        words: blockEntites,
+        start: blockEntites[0].start
+      },
+      entityRanges: generateEntitiesRanges(blockEntites, 'punct'),
+    };
+
+    updatedBlockArray.push(updatedBlock);
+    totalWords += wordsInBlock;
+  }
+
+  return { blocks: updatedBlockArray, entityMap: createEntityMap(updatedBlockArray) };
+};
+
 // https://github.com/google/diff-match-patch/wiki/Line-or-Word-Diffs
 const diffLineMode = (text1, text2) => {
   var dmp = new DiffMatchPatch();
@@ -38,6 +67,7 @@ const diffLineMode = (text1, text2) => {
   return diffs;
 };
 
+// Update timestamps usign diff-match-patch.
 const updateTimestamps = (currentContent, originalContent) => {
   const currentText = convertContentToText(currentContent);
   const originalText = convertContentToText(originalContent);
@@ -112,33 +142,34 @@ const updateTimestamps = (currentContent, originalContent) => {
     diffIdx ++;
   }
 
-  // Update entites to block structure.
-  var updatedBlockArray = [];
-  var totalWords = 0;
-
-  for (var blockIdx in currentContent.blocks) {
-    const block = currentContent.blocks[blockIdx];
-    const wordsInBlock = (block.text.match(/\S+/g) || []).length;
-    const blockEntites = newEntities.slice(totalWords, totalWords + wordsInBlock);
-
-    const updatedBlock = {
-      text: blockEntites.map((entry) => entry.punct).join(' '),
-      type: 'paragraph',
-      data: {
-        speaker: block.data.speaker,
-        words: blockEntites,
-        start: blockEntites[0].start
-      },
-      entityRanges: generateEntitiesRanges(blockEntites, 'punct'),
-    };
-
-    updatedBlockArray.push(updatedBlock);
-    totalWords += wordsInBlock;
-  }
-
-  const updatedContent = { blocks: updatedBlockArray, entityMap: createEntityMap(updatedBlockArray) };
+  const updatedContent = createContentFromEntityList(currentContent, newEntities);
 
   return updatedContent;
 };
 
-export default updateTimestamps;
+// Update timestamps usign stt-align (bbc).
+const updateTimestampsSSTAlign = (currentContent, originalContent) => {
+  const currentText = convertContentToText(currentContent);
+
+  const entityMap = originalContent.entityMap;
+
+  const entities = [];
+
+  for (var entityIdx in entityMap) {
+    entities.push({
+      start: parseFloat(entityMap[entityIdx].data.start),
+      end: parseFloat(entityMap[entityIdx].data.end),
+      word: entityMap[entityIdx].data.text.toLowerCase().replace(/[.?!]/g, ''),
+    });
+  }
+
+  const result = alignJSONText( { words: entities }, currentText.join(' '));
+  const newEntities = result.words.map((entry) => {
+    return createEntity(entry.start, entry.end, 0.0, entry.word, -1);
+  });
+  const updatedContent = createContentFromEntityList(currentContent, newEntities);
+
+  return updatedContent;
+};
+
+export { updateTimestamps, updateTimestampsSSTAlign };
