0742cdb5ca1c44f4ca0b876d6f85e270e231c75b
Pietro Passarelli - News Labs <pietro.passarelli@bbc.co.uk>
2019-04-29 10:34:47 +0100

    fixed export btn in firefox

--- demo/app.js
+++ demo/app.js
@@ -100,6 +100,7 @@ class App extends React.Component {
   };
 
   exportTranscript = () => {
+    console.log('export');
     // eslint-disable-next-line react/no-string-refs
     const { data, ext } = this.transcriptEditorRef.current.getEditorContent(
       this.state.exportFormat
@@ -109,13 +110,18 @@ class App extends React.Component {
 
   // https://stackoverflow.com/questions/2897619/using-html5-javascript-to-generate-and-save-a-file
   download = (content, filename, contentType) => {
+    console.log('download');
     const type = contentType || 'application/octet-stream';
     const link = document.createElement('a');
     const blob = new Blob([ content ], { type: type });
 
     link.href = window.URL.createObjectURL(blob);
     link.download = filename;
+    // Firefox fix - cannot do link.click() if it's not attached to DOM in firefox
+    // https://stackoverflow.com/questions/32225904/programmatical-click-on-a-tag-not-working-in-firefox
+    document.body.appendChild(link);
     link.click();
+    document.body.removeChild(link);
   };
 
   clearLocalStorage = () => {
