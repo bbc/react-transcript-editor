968e5707bff567bc435ef9ee6f5ea998e4e0e675
murezzda <47388020+murezzda@users.noreply.github.com>
2019-06-10 18:55:12 +0200

    Changed time of timestamp-update. Now re-calculates the timestamps after 5 seconds if the transcript has been edited or if the user saves the transcript manually with the save button

--- packages/components/timed-text-editor/index.js
+++ packages/components/timed-text-editor/index.js
@@ -101,6 +101,13 @@ class TimedTextEditor extends React.Component {
           }.bind(this), pauseWhileTypingIntervalInMilliseconds);
         }
       }
+
+      if (this.timestampTimer !== undefined) {
+        clearTimeout(this.timestampTimer);
+      }
+      this.timestampTimer = setTimeout(() => {
+        this.updateTimestampsForEditorState();
+      }, 5000);
     }
 
     if (this.state.isEditable) {
@@ -112,14 +119,14 @@ class TimedTextEditor extends React.Component {
           clearTimeout(this.saveTimer);
         }
         this.saveTimer = setTimeout(() => {
-          this.updateTimestampsForEditorState();
           this.localSave(this.props.mediaUrl);
-        }, 5000);
+        }, 1000);
       });
     }
   }
 
   updateTimestampsForEditorState() {
+
     // Update timestamps according to the original state.
     const currentContent = convertToRaw(this.state.editorState.getCurrentContent());
     const updatedContentRaw = updateTimestampsSSTAlign(currentContent, this.state.originalState);
@@ -150,7 +157,6 @@ class TimedTextEditor extends React.Component {
 
     // Set the updated selection state on the new editor state
     const newEditorStateSelected = EditorState.forceSelection(newEditorState, selection);
-
     this.setState({ editorState: newEditorStateSelected });
   }
 
