9d725b7d412222f44a0ccc111b880f70565335d9
Pietro Passarelli - News Labs <pietro.passarelli@bbc.co.uk>
2019-07-31 23:05:36 +0100

    fixed test
    added sample files for adding tests for subtitle composer module at later stage

--- packages/export-adapters/subtitles-generator/presegment-text/index.test.js
+++ packages/export-adapters/subtitles-generator/presegment-text/index.test.js
@@ -1,9 +1,9 @@
 import fs from 'fs';
 import preSegmentText from './index.js';
 // requrie on js and json is relative to current file path
-import { words as sampleWords } from '../../sample/words-list.sample.json';
-// fs is relative to where the node process start
-const sampleSegmentedOutput = fs.readFileSync('./example-output/test-presegment.txt').toString();
+import { words as sampleWords } from '../sample/words-list.sample.json';
+// fs path is relative to where the node process start
+const sampleSegmentedOutput = fs.readFileSync('./packages/export-adapters/subtitles-generator/sample/test-presegment.sample.txt').toString();
 
 const numberOfCharPerLine35 = 35;
 
